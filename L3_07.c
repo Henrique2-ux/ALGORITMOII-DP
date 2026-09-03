fc8ad0a644056defcff89f4ec3092b2ea3ddcd10
Arthur Henrique <arthur.ribas456@gmail.com>
2026-09-03 08:19:57 -0400

    Update L3_07.c

--- L3_07.c
+++ L3_07.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+void bubbleSort(int vetor[], int tamanho) {
+    int temp;
 
-void bubbleSort(int *vetor, int tamanho) {
-    int *p, *q;
-
-    for (p = vetor; p < vetor + tamanho - 1; p++) {
-        for (q = vetor; q < vetor + tamanho - 1 - (p - vetor); q++) {
-            if (*q > *(q + 1)) {
-                swap(q, q + 1);
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - 1 - i; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
             }
         }
     }
@@ -20,17 +16,15 @@ void bubbleSort(int *vetor, int tamanho) {
 
 int main() {
     int vetor[] = {5, 2, 9, 1, 3};
-    int tamanho = sizeof(vetor) / sizeof(vetor[0]);
+    int tamanho = 5;
 
     bubbleSort(vetor, tamanho);
 
     printf("Vetor ordenado: ");
 
-    for (int *p = vetor; p < vetor + tamanho; p++) {
-        printf("%d ", *p);
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
     }
-
-    printf("\n");
 
     return 0;
 }
