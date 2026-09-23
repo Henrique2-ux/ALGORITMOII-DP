df82a6cc0fd2e998e19637be00e040e833a82c3c
Arthur Henrique <arthur.ribas456@gmail.com>
2026-09-23 06:17:19 -0400

    Create 01_03.c

---
+++ 01_03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void converterTempo(int segundos)
+{
+    int horas, minutos;
+  
+    horas = segundos / 3600;
+    segundos = segundos % 3600;
+    minutos = segundos / 60;
+    segundos = segundos % 60;
+
+    printf("%d horas, %d minutos e %d segundos\n", horas, minutos, segundos);
+}
+
+int main()
+{
+    int tempo;
+  
+    printf("Digite o tempo de duracao da fabrica em segundos: ");
+    scanf("%d", &tempo);
+  
+    converterTempo(tempo);
+    return 0;
+}
