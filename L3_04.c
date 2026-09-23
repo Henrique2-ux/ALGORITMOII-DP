0aa19cbee7949cbcde6dfa1e85764e77c0d04073
Arthur Henrique <arthur.ribas456@gmail.com>
2026-09-23 06:29:18 -0400

    Update L3_04.c

--- L3_04.c
+++ L3_04.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-/* Crie uma função void inverte_vetor(int *vetor, int tamanho) que inverta a ordem dos
-elementos de um vetor original.
--Você deve criar dois ponteiros locais dentro da função: um inicio apontando
-para o primeiro elemento e um fim apontando para o último.
-Faça um laço onde o inicio avança (++) e o fim recua (--) trocando os valores de
-lugar até que os ponteiros se cruzem no meio do vetor. Não utilize variáveis
-inteiras como índice.
-*/
-
 void inverte_vetor(int *vetor, int tamanho){
 
     int *inicio = vetor;
